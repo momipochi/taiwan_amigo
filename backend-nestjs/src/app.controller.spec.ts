aafe1cf35b349ee651832741a51dbeeac8d70b04
momipochi <k25650727@gmail.com>
2023-06-09 12:01:24 +0200

    update test

--- backend-nestjs/src/app.controller.spec.ts
+++ backend-nestjs/src/app.controller.spec.ts
@@ -1,22 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelemetryService } from './services/telemetry/telemetry.service';
 
-// describe('AppController', () => {
-//   let appController: AppController;
+describe('AppController', () => {
+  let appController: AppController;
 
-//   beforeEach(async () => {
-//     const app: TestingModule = await Test.createTestingModule({
-//       controllers: [AppController],
-//       providers: [AppService],
-//     }).compile();
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService, TelemetryService],
+    }).compile();
 
-//     appController = app.get<AppController>(AppController);
-//   });
+    appController = app.get<AppController>(AppController);
+  });
 
-//   describe('root', () => {
-//     it('should return "Hello World!"', () => {
-//       expect(appController.getHello()).toBe('Hello World!');
-//     });
-//   });
-// });
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
--- backend-nestjs/src/services/telemetry/telemetry.service.spec.ts
+++ backend-nestjs/src/services/telemetry/telemetry.service.spec.ts
@@ -1,21 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TelemetryService } from './telemetry.service';
-import * as ColorCodedMessages from 'src/utility/colorCodedMessages/colorCodedMessages';
 
-// describe('TelemetryService', () => {
-//   let service: TelemetryService;
-//   jest.mock('src/utility/colorCodedMessages/colorCodedMessages', jest.fn());
-//   const spy = jest.spyOn(ColorCodedMessages.ColorCodedMessages, 'RedMessage');
-//   spy.mockImplementation(jest.fn());
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [TelemetryService],
-//     }).compile();
+describe('TelemetryService', () => {
+  let service: TelemetryService;
 
-//     service = module.get<TelemetryService>(TelemetryService);
-//   });
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TelemetryService],
+    }).compile();
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+    service = module.get<TelemetryService>(TelemetryService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
