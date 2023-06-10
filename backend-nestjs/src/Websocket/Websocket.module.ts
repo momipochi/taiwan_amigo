db6044c22afe69624ea0339321690c6a19992967
z7016187 <asd880810@gmail.com>
2023-06-10 18:54:40 +0800

    bnn

---
+++ backend-nestjs/src/Websocket/Websocket.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { MyWebsocket } from "./Websocket";
+
+
+@Module({
+    providers: [MyWebsocket]
+})
+export class WebsocketModule {
+
+
+   
+}
--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelemetryService } from './services/telemetry/telemetry.service';
+import { WebsocketModule } from './Websocket/Websocket.module';
 
 @Module({
-  imports: [],
+  imports: [WebsocketModule],
   controllers: [AppController],
   providers: [AppService, TelemetryService],
 })
