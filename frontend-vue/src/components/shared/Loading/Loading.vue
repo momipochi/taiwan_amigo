9772c9a4582363bb3bb7e84194e28818f7b914a5
momipochi <k25650727@gmail.com>
2023-06-21 13:36:52 +0200

    added loading animation

---
+++ frontend-vue/src/components/shared/Loading/Loading.vue
@@ -0,0 +1,7 @@
+<template>
+  <span class="loader-circle"></span>
+</template>
+
+<style lang="scss">
+@import "./loadingStyle.scss";
+</style>
---
+++ frontend-vue/src/components/shared/Loading/LoadingText.vue
@@ -0,0 +1,6 @@
+<template>
+  <span class="loader-text">讀取中</span>
+</template>
+<style lang="scss">
+@import "./loadingTextStyle.scss";
+</style>
