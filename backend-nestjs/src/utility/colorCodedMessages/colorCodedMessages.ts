a08d154fe1bc3b45559018cb6669d5b4e00927e6
z7016187 <asd880810@gmail.com>
2023-08-10 00:29:43 +0800

    added docker

--- backend-nestjs/src/utility/colorCodedMessages/colorCodedMessages.ts
+++ backend-nestjs/src/utility/colorCodedMessages/colorCodedMessages.ts
@@ -2,7 +2,7 @@ export class ColorCodedMessages {
   static RedMessage(message: string) {
     console.error(`\u001b[31m${message}\u001b[0m`);
   }
-  static GreenMessage(message: string) {
+  // static GreenMessage(message: string) {
 
-  }
+  // }
 }
