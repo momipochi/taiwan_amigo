a10e7d5aa9dc0623860e28c3e69bfb283bb04049
momipochi <k25650727@gmail.com>
2023-08-15 18:06:53 +0200

    commit

--- frontend-vue/src/shared/constants/links/links.ts
+++ frontend-vue/src/shared/constants/links/links.ts
@@ -1,4 +1,5 @@
-export const DOMAIN_URL = "http://localhost:3000/";
+// export const DOMAIN_URL = "http://localhost:3000/";
+export const DOMAIN_URL = "https://610amig.azurewebsite.net";
 // export const DOMAIN_URL = "https://192.168.147.30:3001/";
 // export const DOMAIN_URL = "https://192.168.0.103:3001/";
 
