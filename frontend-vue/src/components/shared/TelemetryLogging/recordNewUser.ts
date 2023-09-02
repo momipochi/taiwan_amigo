2947fd70e2982a0fe7659c5105bc82e58dc4db5a
momipochi <k25650727@gmail.com>
2023-09-02 13:33:12 +0200

    modified frontend

--- frontend-vue/src/components/shared/TelemetryLogging/recordNewUser.ts
+++ frontend-vue/src/components/shared/TelemetryLogging/recordNewUser.ts
@@ -1,7 +1,16 @@
 import { baseFetch } from "../../../shared/api/baseFetchApi/baseFetchApi";
 import { API_ROUTES } from "../../../shared/constants/links/links";
 
+const LAST_ACCESS = "lastAccessed";
+
 export const recordNewUser = async () => {
+  const data = localStorage.getItem(LAST_ACCESS);
+  const date = new Date();
+  const today = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`;
+  if(data === today){
+    return  
+  }
+  localStorage.setItem(LAST_ACCESS,today)
   try {
     const response = await baseFetch(API_ROUTES.LOG_NEW_USER, "PUT");
     if (response?.status !== 200) {
