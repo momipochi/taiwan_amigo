e39662fb3d549816c41a0cc16392978de46916c8
momipochi <k25650727@gmail.com>
2023-09-08 18:30:13 +0200

    commit

--- frontend-vue/src/components/ChatRoom/ChatRoom.vue
+++ frontend-vue/src/components/ChatRoom/ChatRoom.vue
@@ -90,8 +90,9 @@ type ToggleStatus = "neutral" | "showVideo" | "showChat";
               <div id="messaging">
                 <div id="typing-area">
                   <input
-                    
-                    v-bind:disabled="!webRTCState.loadingOpponent"
+                  v-on:focus="()=>{chatIsFocused = true}"
+              v-on:blur="()=>{chatIsFocused = false}"
+                    v-bind:disabled="webRTCState.loadingOpponent"
                     type="text"
                     placeholder="說點什麼..."
                     v-on:keyup.enter="onSendMessage"
@@ -165,8 +166,7 @@ type ToggleStatus = "neutral" | "showVideo" | "showChat";
           </div>
           <div id="chat-buttons">
             <button
-              v-on:focus="()=>{chatIsFocused = true}"
-              v-on:blur="()=>{chatIsFocused = false}"
+              
               v-if="webRTCState.loadingOpponent"
               id="next-person"
               v-on:click="connectWithNextUser"
