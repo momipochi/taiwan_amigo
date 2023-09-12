048f87df5b53f11e0079e063b3ca80063e8f40e7
momipochi <k25650727@gmail.com>
2023-09-13 00:09:56 +0200

    commit

--- frontend-vue/vite.config.ts
+++ frontend-vue/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
+import generateSitemap from "vite-plugin-pages-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Pages({
+      onRoutesGenerated(routes) {
+        generateSitemap({
+          hostname: "https://6tentalk.com/",
+          routes,
+          allowRobots: true,
+        });
+      },
+    }),
+  ],
 });
