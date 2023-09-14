9e9d3bcf72b1f134511854bf24ee40fee92eb96b
momipochi <k25650727@gmail.com>
2023-09-14 14:05:01 +0200

    commit

--- frontend-vue/src/shared/constants/links/links.ts
+++ frontend-vue/src/shared/constants/links/links.ts
@@ -1,11 +1,7 @@
-// export const DOMAIN_URL = "http://localhost:8000/";
-// export const DOMAIN_URL = "https://610amigo.azurewebsites.net/";
-const PROD_URL = import.meta.env.BASE_URL
-const DEV_URL = "http://localhost:8000/";
-export const DOMAIN_URL =  PROD_URL ? PROD_URL : DEV_URL
-// export const DOMAIN_URL = "https://192.168.147.30:3001/";
-// export const DOMAIN_URL = "https://192.168.0.103:3001/";
 
+const PROD_URL = "https://610amigo.azurewebsites.net/"
+const DEV_URL = "http://localhost:8000/";
+export const DOMAIN_URL =  PROD_URL
 export const API_ROUTES = {
   LOG_NEW_USER: "Wassup",
   TOTAL_WAIT_TIME: "totalWaitTime"
